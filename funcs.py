cc6d174d8c24ea70a4b00ab60e8ec9dc41204f68
fedor <fedorusov100@yandex.ru>
2018-11-14 18:21:21 +0300

    minor changes

---
+++ funcs.py
@@ -0,0 +1,7 @@
+from functools import partial
+from math import sin, cos
+
+S_funcs = ["a * t + b * sin(c * t)"]
+z_funcs = ["a * t + b * cos(c * t)"]
+p_funcs = ["a * w * (1 - w)"]
+f_funcs = ["b (x - z)"]
